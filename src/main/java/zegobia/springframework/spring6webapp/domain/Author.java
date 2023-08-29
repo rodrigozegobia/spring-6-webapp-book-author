0255b72a3ff0aad7ca59b761684e5a476ad52361
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-08-29 18:26:29 -0300

    Implements equals and hashCode

--- src/main/java/zegobia/springframework/spring6webapp/domain/Author.java
+++ src/main/java/zegobia/springframework/spring6webapp/domain/Author.java
@@ -46,4 +46,27 @@ public class Author {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+
+        return getId() != null ? getId().equals(author.getId()) : author.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
 }
