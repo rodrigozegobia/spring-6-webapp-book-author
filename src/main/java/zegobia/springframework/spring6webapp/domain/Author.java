919da32db97753b7f036540dea03b626539a008b
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-08-29 16:01:07 -0300

    Initial commit

---
+++ src/main/java/zegobia/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,48 @@
+package zegobia.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastname;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
