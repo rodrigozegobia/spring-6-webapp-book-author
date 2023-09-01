c81dae3c3c3446d96f44515c803f230431af5de6
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-09-01 11:33:57 -0300

    Create PublisherRepository.java

---
+++ src/main/java/zegobia/springframework/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package zegobia.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import zegobia.springframework.spring6webapp.domain.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
