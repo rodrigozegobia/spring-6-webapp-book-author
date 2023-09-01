3ff2eac0dbc34bc40d990c86586e5c0dcb6c2bd3
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-09-01 11:33:38 -0300

    Adds Hashset to book and author

--- src/main/java/zegobia/springframework/spring6webapp/domain/Author.java
+++ src/main/java/zegobia/springframework/spring6webapp/domain/Author.java
@@ -1,6 +1,8 @@
 package zegobia.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
+
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,7 +15,7 @@ public class Author {
     private String lastname;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Set<Book> getBooks() {
         return books;
